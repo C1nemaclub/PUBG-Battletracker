f943c702ff53839dd3423ccbd9df32dc87888914
Santiago <santiagov801@gmail.com>
2022-05-02 16:10:57 -0500

    Contact form using Nodemailer and Ethereal is functional

--- routes/info.js
+++ routes/info.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const nodemailer = require('nodemailer');
+require('dotenv').config();
 
 router.use('/contact', (req, res) => {
   res.render('contact');
@@ -7,6 +9,33 @@ router.use('/contact', (req, res) => {
 
 router.use('/about', (req, res) => {
   res.render('about');
+});
+
+router.post('/', (req, res) => {
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.ethereal.email',
+    port: 587,
+    auth: {
+      user: process.env.USER,
+      pass: process.env.PASSWORD,
+    },
+  });
+
+  const mailOptions = {
+    name: req.body.sender,
+    from: req.body.email,
+    to: process.env.EMAIL,
+    subject: req.body.subject,
+    text: req.body.messagge,
+  };
+
+  transporter.sendMail(mailOptions, function (err, info) {
+    if (err) console.log(err);
+    else console.log(info);
+  });
+
+  console.log(req.body);
+  res.redirect('/info/contact');
 });
 
 module.exports = router;
